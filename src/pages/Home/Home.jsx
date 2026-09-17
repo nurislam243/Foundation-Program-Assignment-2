b293663ec64030bf38cae09069fd05cb54db741f
Nur Islam <mdnurislam15177@gmail.com>
2026-09-17 19:12:02 +0600

    feat: added Home.jsx

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router";
+import { FiSearch } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <div
+      className="hero min-h-[85vh] relative bg-cover bg-center"
+      style={{
+        backgroundImage:
+          "url('https://i.ibb.co.com/N6Mkpm3X/wp12389567-4k-cinema-wallpapers.webp')",
+      }}
+    >
+      <div className="hero-overlay bg-slate-950/70"></div>
+
+      {/* Content */}
+      <div className="hero-content justify-start w-full max-w-7xl  text-white z-10">
+        <div className="max-w-xl space-y-6">
+          <h1 className="text-4xl md:text-6xl font-extrabold tracking-tight leading-tight">
+            Discover <br />
+            Amazing <span className="text-primary">Movies</span>
+          </h1>
+
+          <p className="text-gray-300 text-lg md:text-xl font-normal leading-relaxed">
+            Explore and discover your favorite movies from around the world.
+          </p>
+
+          <div>
+            <Link
+              to="/movies"
+              className="btn btn-primary rounded-xl shadow-lg flex items-center gap-2 w-fit text-base px-8"
+            >
+              <FiSearch className="text-xl" />
+              Explore Now
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
